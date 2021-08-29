518c4d2b7752820ff72f78cf64332c92f89679b3
neeleshguptaaws <neeleshguptaaws@gmail.com>
2021-08-29 14:45:49 +0530

    first commit

---
+++ src/test/java/com/neel/NeelRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeelRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
